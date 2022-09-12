2ffdfc814bb2abb152385c6cf668992fd795e00b
An Nguyen <shennguyen.redshinobi96@gmail.com>
2022-09-12 11:12:40 +0300

    fix: update saga and jobDetails com to get applied job information.

--- src/components/jobseekers/appliedJobs.component.js
+++ src/components/jobseekers/appliedJobs.component.js
@@ -1,63 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import { Grid, Paper, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import ReactPaginate from 'react-paginate';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import { customURL, dateFormat } from '../../utils/helperFunctions';
-import axios from 'axios';
-import { fetchJobApplicants, fetchJobById } from '../../actions';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { Grid, Paper, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import ReactPaginate from "react-paginate";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import { customURL, dateFormat } from "../../utils/helperFunctions";
+import axios from "axios";
+import { fetchJobApplicants, fetchJobById } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
 
-const AppliedJobsComponent = ({ appliedJobs, advertPages, changeAdvertPage, selectedPage }) => {
-  const { t } = useTranslation('appliedJobs', 'common');
+const AppliedJobsComponent = ({
+  appliedJobs,
+  advertPages,
+  changeAdvertPage,
+  selectedPage,
+}) => {
+  const { t } = useTranslation("appliedJobs", "common");
 
   return (
     <div className="container" key={appliedJobs.id}>
       <div>
-        <Grid container style={{ padding: '50px 0px' }}>
+        <Grid container style={{ padding: "50px 0px" }}>
           <Grid item sm={10}>
             <h3>
-              {t('appliedJobsTitle')}({`${appliedJobs.length}`}):
+              {t("appliedJobsTitle")}({`${appliedJobs.length}`}):
             </h3>
           </Grid>
         </Grid>
       </div>
 
       {appliedJobs &&
-        appliedJobs.slice(selectedPage * 10, selectedPage * 10 + 10).map(post => {
-          return (
-            <div key={`${post.jobPostId}${post.id}`}>
-              <Paper style={{ marginTop: 20 }}>
-                <Grid container spacing={1} style={{ padding: 20 }} alignItems="center">
-                  <Grid item xs={8}>
-                    <div>
-                      <Link to={customURL(post.url, 'external')} className="btnLink">
-                        <h4>{post.jobTitle}</h4>
-                      </Link>
-                    </div>
-                    <div>
-                      <span>
-                        {t('common:deadline')}:
-                        <span style={{ color: 'red', margin: '0 5px' }}>{post.closingDate?.indexOf(':00.000Z') ? dateFormat(post.closingDate) : post.closingDate}</span>
-                      </span>
-                    </div>
+        appliedJobs
+          .slice(selectedPage * 10, selectedPage * 10 + 10)
+          .map((post) => {
+            return (
+              <div key={`${post.jobPostId}${post.id}`}>
+                <Paper style={{ marginTop: 20 }}>
+                  <Grid
+                    container
+                    spacing={1}
+                    style={{ padding: 20 }}
+                    alignItems="center"
+                  >
+                    <Grid item xs={8}>
+                      <div>
+                        <Link
+                          to={customURL(post.url, "external")}
+                          className="btnLink"
+                        >
+                          <h4>{post.jobTitle}</h4>
+                        </Link>
+                      </div>
+                      <div>
+                        <span>
+                          {t("common:deadline")}:
+                          <span style={{ color: "red", margin: "0 5px" }}>
+                            {post.closingDate?.indexOf(":00.000Z")
+                              ? dateFormat(post.closingDate)
+                              : post.closingDate}
+                          </span>
+                        </span>
+                      </div>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <div style={{ textAlign: "right" }}>
+                        <Link
+                          to={customURL(post.url, "external")}
+                          className="btnLink"
+                        >
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            // onClick={() => fetchJobById(post.id, post.jobPostId)}
+                          >
+                            {t("common:openBtn")}
+                          </Button>
+                        </Link>
+                      </div>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={4}>
-                    <div style={{ textAlign: 'right' }}>
-                      <Link to={customURL(post.url, 'internal')} className="btnLink">
-                        <Button variant="contained" color="primary"  onClick={() => fetchJobById(post.id)}>
-                          {t('common:openBtn')}
-                        </Button>
-                      </Link>
-                    </div>
-                  </Grid>
-                </Grid>
-              </Paper>
-            </div>
-          );
-        })}
+                </Paper>
+              </div>
+            );
+          })}
       <div className="pagination-body">
         <ReactPaginate
           previousLabel={<NavigateBeforeIcon />}
--- src/containers/jobs/jobDetails.container.js
+++ src/containers/jobs/jobDetails.container.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import JobDetailsComponent from '../../components/jobs/jobDetails.component';
-import { getJobDetailsById, toggleFavoriteJobs, closeSnackbar, changePagination,fetchJobById,fetchJobInfo,setIdToApply } from '../../actions';
+import React from "react";
+import { connect } from "react-redux";
+import JobDetailsComponent from "../../components/jobs/jobDetails.component";
+import {
+  getJobDetailsById,
+  toggleFavoriteJobs,
+  closeSnackbar,
+  changePagination,
+  fetchJobById,
+  fetchJobInfo,
+  setIdToApply,
+} from "../../actions";
 
 class JobDetailsContainer extends React.Component {
   componentDidMount() {
-    this.props.getJobDetailsById(`${this.props.match.params.id}$$${this.props.match.params.companyId}`);
+    this.props.getJobDetailsById(
+      `${this.props.match.params.jobtitle}/${this.props.match.params.id}`
+    );
   }
 
   render() {
@@ -13,13 +23,15 @@ class JobDetailsContainer extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     state,
-    jobDetails: state.jobs.jobsList && state.jobs.jobsList,
+    jobDetails: state.jobs.jobDetails && state.jobs.jobDetails,
     workStart: state.jobs.workStart && state.jobs.workStart,
     favoriteJobs: state.jobs.favoriteJobs && state.jobs.favoriteJobs,
-    favBtnstatus: !!(state.jobs.favoriteJobs && state.jobs.favoriteJobs.isFavourite),
+    favBtnstatus: !!(
+      state.jobs.favoriteJobs && state.jobs.favoriteJobs.isFavourite
+    ),
     showSuccessSnackbar: state.asyncActions.showSuccessSnackbar,
     showFailedSnackbar: state.asyncActions.showFailedSnackbar,
     showDialog: state.asyncActions.showDialog,
@@ -33,7 +45,10 @@ const mapDispatchToProps = {
   changePagination,
   fetchJobById,
   fetchJobInfo,
-  setIdToApply
+  setIdToApply,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(JobDetailsContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(JobDetailsContainer);
